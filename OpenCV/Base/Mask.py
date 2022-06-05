4edbaae35ee624f824ebc01e904f62fe97038fa9
Jose-Alves-Neto <jose.alves.neto@ccc.ufcg.edu.br>
2022-06-05 20:51:47 -0300

    Criação do rastreamento do objeto

--- OpenCV/Base/Mask.py
+++ OpenCV/Base/Mask.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 max_value = 255
 max_value_H = 360//2
@@ -16,9 +17,7 @@ min_H_name, max_H_name = "Hmin", "Hmax"
 min_S_name, max_S_name = "Smin", "Smax"
 min_V_name, max_V_name = "Vmin", "Vmax"
 
-
-def nothing(x):
-    pass
+record = False
 
 
 def constrain(val, min_val, max_val):
@@ -29,6 +28,24 @@ def resize(img, amount):
     width = int(img.shape[1] * amount/100)
     height = int(img.shape[0] * amount/100)
     return cv.resize(img, (width, height), interpolation=cv.INTER_AREA)
+
+
+def writePos(x, y, angle):
+    with open("pos.txt", "a") as f:
+        f.write("\n" + str(x) + " " + str(y) + " " + str(angle * 180 / np.pi))
+
+
+def plot():
+    with open("pos.txt", "r") as f:
+        x, y = [], []
+        for line in f:
+            if line == "\n":
+                continue
+            x.append(int(line.split()[0]))
+            y.append(int(line.split()[1]))
+        plt.plot(x, y, 'ro')
+        plt.axis([0, 640, 480, 0])
+        plt.show()
 
 
 def on_low_H_thresh_trackbar(val):
@@ -49,12 +66,49 @@ def on_max_H_thresh_trackbar(val):
         cv.setTrackbarPos(min_H_name, window_mask, val - 1)
 
 
+def on_low_S_thresh_trackbar(val):
+    global s_min
+    s_max = cv.getTrackbarPos(max_S_name, window_mask)
+    s_min = constrain(val, 0, s_max-1)
+    cv.setTrackbarPos(min_S_name, window_mask, s_min)
+    if (val > s_max):
+        cv.setTrackbarPos(max_S_name, window_mask, val + 1)
+
+
+def on_max_S_thresh_trackbar(val):
+    global s_max
+    s_min = cv.getTrackbarPos(min_S_name, window_mask)
+    s_max = constrain(val, s_min, max_value)
+    cv.setTrackbarPos(max_S_name, window_mask, s_max)
+    if (val < s_min):
+        cv.setTrackbarPos(min_S_name, window_mask, val - 1)
+
+
+def on_min_V_thresh_trackbar(val):
+    global v_min
+    v_max = cv.getTrackbarPos(max_V_name, window_mask)
+    v_min = constrain(val, 0, v_max-1)
+    cv.setTrackbarPos(min_V_name, window_mask, v_min)
+    if (val > v_max):
+        cv.setTrackbarPos(max_V_name, window_mask, val + 1)
+
+
+def on_max_V_thresh_trackbar(val):
+    global v_max
+    v_min = cv.getTrackbarPos(min_V_name, window_mask)
+    v_max = constrain(val, v_min, max_value)
+    cv.setTrackbarPos(max_V_name, window_mask, v_max)
+    if (val < v_min):
+        cv.setTrackbarPos(min_V_name, window_mask, val - 1)
+
+
 def getMaskValue():
     return np.array([h_min, s_min, v_min]), np.array([h_max, s_max, v_max])
 
 
 captura = cv.VideoCapture(0, cv.CAP_MSMF)
-
+captura.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+captura.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 window_mask = 'Colorbars'
 
 cv.namedWindow(window_mask)
@@ -64,13 +118,17 @@ cv.createTrackbar(min_H_name, window_mask, h_min,
 cv.createTrackbar(max_H_name, window_mask, h_max,
                   max_value_H, on_max_H_thresh_trackbar)
 
-cv.createTrackbar(min_S_name, window_mask, s_min, max_value, nothing)
-cv.createTrackbar(max_S_name, window_mask, s_max, max_value, nothing)
+cv.createTrackbar(min_S_name, window_mask, s_min,
+                  max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar(max_S_name, window_mask, s_max,
+                  max_value, on_max_S_thresh_trackbar)
 
-cv.createTrackbar(min_V_name, window_mask, v_min, max_value, nothing)
-cv.createTrackbar(max_V_name, window_mask, v_max, max_value, nothing)
+cv.createTrackbar(min_V_name, window_mask, v_min,
+                  max_value, on_min_V_thresh_trackbar)
+cv.createTrackbar(max_V_name, window_mask, v_max,
+                  max_value, on_max_V_thresh_trackbar)
 
-kernel = np.ones((5, 5), np.uint8)
+kernel = np.ones((2, 3), np.uint8)
 
 while(True):
     ret, frame = captura.read()
@@ -82,7 +140,7 @@ while(True):
     minMask, maxMask = getMaskValue()
     mask = cv.inRange(hsv, minMask, maxMask)
     frameMask = cv.bitwise_and(hsv, frame, mask=mask)
-    result = cv.dilate(frameMask, kernel, iterations=2)
+    result = cv.dilate(frameMask, kernel, iterations=1)
 
     (cnts, _) = cv.findContours(cv.cvtColor(result.copy(), cv.COLOR_BGR2GRAY),
                                 cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -90,9 +148,19 @@ while(True):
         continue
     elif len(cnts) > 0:
         cnt = sorted(cnts, key=cv.contourArea, reverse=True)[0]
+        if record:
+            M = cv.moments(cnt)
+            if (M['m00'] != 0):
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                cv.circle(frame, (cx, cy), 7, (0, 0, 255), -1)
+                coords = cv.boxPoints(cv.minAreaRect(cnt))
+                angle = np.arctan2(
+                    coords[3][1] - coords[1][1], coords[3][0] - coords[1][0])
+                writePos(cx, cy, angle)
+
         rect = np.int32(cv.boxPoints(cv.minAreaRect(cnt)))
-        cv.drawContours(frame, [rect], -1, (0, 255, 255),
-                        2)
+        cv.drawContours(frame, [rect], -1, (0, 255, 255), 2)
         cv.imshow("conto", frame)
 
     Vert = np.vstack([
@@ -104,7 +172,9 @@ while(True):
 
     k = cv.waitKey(30) & 0xff
     if k == 27:
-        print(cv.getWindowImageRect('Colorbars'))
+        plot()
         break
+    elif k == ord('r'):
+        record = not record
 
 cv.destroyAllWindows()
