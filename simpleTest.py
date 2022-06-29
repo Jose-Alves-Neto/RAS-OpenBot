f79af5d3289e5a8259fc634a6270d5b4ba683498
José Alves <jose.alves.neto@ccc.ufcg.edu.br>
2022-06-29 15:10:09 -0300

    Add files via upload

---
+++ simpleTest.py
@@ -0,0 +1,61 @@
+# Make sure to have the server side running in CoppeliaSim: 
+# in a child script of a CoppeliaSim scene, add following command
+# to be executed just once, at simulation start:
+#
+# simRemoteApi.start(19999)
+#
+# then start simulation, and run this program.
+#
+# IMPORTANT: for each successful call to simxStart, there
+# should be a corresponding call to simxFinish at the end!
+
+try:
+    import sim
+except:
+    print ('--------------------------------------------------------------')
+    print ('"sim.py" could not be imported. This means very probably that')
+    print ('either "sim.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "sim.py"')
+    print ('--------------------------------------------------------------')
+    print ('')
+
+import time
+linear =0.4
+angular =0.4
+print ('Program started')
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+if clientID!=-1:
+    print ('Connected to remote API server')
+
+    # Now try to retrieve data in a blocking fashion (i.e. a service call):
+    res,objs=sim.simxGetObjects(clientID,sim.sim_handle_all,sim.simx_opmode_blocking)
+    if res==sim.simx_return_ok:
+        print ('Number of objects in the scene: ',len(objs))
+    else:
+        print ('Remote API function call returned with error code: ',res)
+
+    time.sleep(2)
+
+    returnC, rightMotor = sim.simxGetObjectHandle(clientID, "/PioneerP3DX/rightMotor", sim.simx_opmode_oneshot_wait)
+    returnC, leftMotor = sim.simxGetObjectHandle(clientID, "/PioneerP3DX/leftMotor", sim.simx_opmode_oneshot_wait)
+    
+    # Now retrieve streaming data (i.e. in a non-blocking fashion):
+    vLeft=(linear-(0.1905*angular))/0.0975
+    vRight=(linear+(0.1905*angular))/0.0975
+    
+    sim.c_SetJointTargetVelocity(clientID,leftMotor,vLeft,sim.simx_opmode_streaming)
+    sim.c_SetJointTargetVelocity(clientID,rightMotor,vRight,sim.simx_opmode_streaming)
+      
+    # Now send some data to CoppeliaSim in a non-blocking fashion:
+    sim.simxAddStatusbarMessage(clientID,'Hello CoppeliaSim!',sim.simx_opmode_oneshot)
+
+    # Before closing the connection to CoppeliaSim, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+    sim.simxGetPingTime(clientID)
+
+    # Now close the connection to CoppeliaSim:
+    sim.simxFinish(clientID)
+else:
+    print ('Failed connecting to remote API server')
+print ('Program ended')
